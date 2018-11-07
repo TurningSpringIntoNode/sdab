7fe79a00605311e938ff8d311cf5ef596cf804cb
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-11-06 21:30:36 -0300

    Add video field

--- models/episode.model.js
+++ models/episode.model.js
@@ -16,6 +16,10 @@ module.exports = (db, mongoose) => {
       type: String,
       required: true,
     },
+    video: {
+      type: String,
+      required: true,
+    },
     description: {
       type: String,
       required: true,
