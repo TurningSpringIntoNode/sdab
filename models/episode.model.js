88371cdd7c8cde3f7e6fdd7ea4c436f4ce1e4c53
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-11-06 21:25:29 -0300

    Episode model

---
+++ models/episode.model.js
@@ -0,0 +1,30 @@
+
+module.exports = (db, mongoose) => {
+
+  const { Schema } = mongoose;
+
+  const EpisodeSchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    chapter: {
+      type: String,
+      required: true,
+    },
+    thumb: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    anime: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    }
+  });
+
+  db.model('Episode', EpisodeSchema);
+};
